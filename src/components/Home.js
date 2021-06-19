348b30f3ebdd99160d0ca40f780905171a1bd6fa
Satpreet Makhija <sm.satpreet@gmail.com>
2021-06-19 20:17:51 +0530

    added logo to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
-
+import ashokaLogo from '../components/media/ashokaLogo.png';
+import { height, width } from "dom-helpers";
 const Home = () => {
   return (
     <React.Fragment>
       <div className="main-container">
+          <img src = {ashokaLogo} alt="Ashoka Logo" style={{height: "60px", width: "60px"}}/>
         <h1>Check Course Prerequisites hassle-free</h1>
         <h2>Select major from the Navigation bar</h2>
       </div>
